77833db28f776195ec4e6850f4ff35974cdc4d54
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-02 12:27:44 -0500

    add summary to gemspec

---
+++ lib/mbtransfer/transfer.rb
@@ -0,0 +1,16 @@
+module Mbtransfer
+  class Mbtransfer
+    def base_path
+      {"2016": "/Users/TuzsNewMacBook/",
+        "2017": "/Users/TuzMacbookPro2017/"}    
+    end
+
+    def vs_code_path
+      "/Users/TuzsNewMacBook/Library/Application\ Support/Code/User"
+    end
+
+    def icloud_path
+      "Library/Mobile Documents/com~apple~CloudDocs/Development/switching-between-computers"
+    end
+  end
+end
